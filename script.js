50056972750d0cb9c26f1648e9285bbce9189af9
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-10-15 23:02:14 +0300

    Write basic calculator functions

---
+++ script.js
@@ -0,0 +1,22 @@
+function operate(x, y, operator) {
+  if (operator === '+') return add(x, y);
+  if (operator === '-') return subtract(x, y);
+  if (operator === '*') return multiply(x, y);
+  if (operator === '/') return divide(x, y);
+}
+
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
