6e3f86d592c435c6a8badc11f69bff85dfa26f4f
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-10-16 23:33:51 +0300

    Make operators and equal buttons work
    Improve add, subtract, multiply and divide functions
    Now result can't overflow display

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const display = document.querySelector('.display');
 const signBtn = document.querySelector('.sign');
 const inputBtns = document.querySelectorAll('.input');
+const operatorBtns = document.querySelectorAll('.operator');
+const operateBtn = document.querySelector('.operate');
+
+let number = null;
+let operator = null;
+let resetDisplay = false;
 
 inputBtns.forEach((btn) =>
   btn.addEventListener('click', () => {
+    if (resetDisplay) {
+      display.textContent = '0';
+      resetDisplay = false;
+    }
+
     let displayVal = display.textContent;
     const btnVal = btn.textContent;
     const maxChars = displayVal.includes('-') ? 16 : 15;
@@ -21,6 +32,39 @@ inputBtns.forEach((btn) =>
     display.textContent = displayVal + btnVal;
   })
 );
+
+operatorBtns.forEach((btn) =>
+  btn.addEventListener('click', () => {
+    if (number === null) {
+      number = +display.textContent;
+      operator = btn.textContent;
+    } else {
+      const result = operate(number, +display.textContent, operator);
+      display.textContent = result;
+
+      if (result === 'ERROR') {
+        number = null;
+        operator = null;
+      } else {
+        number = result;
+        operator = btn.textContent;
+      }
+    }
+
+    resetDisplay = true;
+  })
+);
+
+operateBtn.addEventListener('click', () => {
+  if (!operator) return;
+
+  const result = operate(number, +display.textContent, operator);
+  display.textContent = result;
+
+  number = null;
+  operator = null;
+  resetDisplay = true;
+});
 
 signBtn.addEventListener('click', () => {
   const displayVal = display.textContent;
@@ -41,17 +85,25 @@ function operate(x, y, operator) {
 }
 
 function add(x, y) {
-  return x + y;
+  return normalizeResult(x + y);
 }
 
 function subtract(x, y) {
-  return x - y;
+  return normalizeResult(x - y);
 }
 
 function multiply(x, y) {
-  return x * y;
+  return normalizeResult(x * y);
 }
 
 function divide(x, y) {
-  return x / y;
+  return normalizeResult(x / y);
+}
+
+function normalizeResult(result) {
+  const unsignedInt = Math.floor(Math.abs(result));
+  const nums = unsignedInt.toString().length;
+
+  if (nums > 15) return 'ERROR';
+  return +result.toFixed(14 - (nums < 13 ? nums : 14));
 }
