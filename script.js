f10a5ec4072c35e15d015225b593cc0c277dc3c6
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2023-10-16 18:02:53 +0300

    Add sign change functionality

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 const display = document.querySelector('.display');
+const signBtn = document.querySelector('.sign');
 const inputBtns = document.querySelectorAll('.input');
 
 inputBtns.forEach((btn) =>
   btn.addEventListener('click', () => {
     let displayVal = display.textContent;
     const btnVal = btn.textContent;
+    const maxChars = displayVal.includes('-') ? 16 : 15;
 
+    if (displayVal.length >= maxChars) return;
     if (btnVal === '0' && displayVal === '0') return;
-    if (btnVal === '.' && displayVal.includes('.')) return;
-    if (displayVal.length >= 15) return;
+    if (
+      btnVal === '.' &&
+      (displayVal.includes('.') || displayVal.length >= maxChars - 1)
+    ) {
+      return;
+    }
+
     if (displayVal === '0' && btnVal !== '.') displayVal = '';
     display.textContent = displayVal + btnVal;
   })
 );
+
+signBtn.addEventListener('click', () => {
+  const displayVal = display.textContent;
+
+  if (displayVal === '0') return;
+  if (displayVal.includes('-')) {
+    display.textContent = displayVal.slice(1);
+  } else {
+    display.textContent = '-' + displayVal;
+  }
+});
 
 function operate(x, y, operator) {
   if (operator === '+') return add(x, y);
